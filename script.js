c6173fe9d642ceb6ee9aab4eb45b789494da4cd0
Pedro Antônio <pedrosilvaantonio635@gmail.com>
2026-05-12 21:19:11 -0300

    abrir camera

---
+++ script.js
@@ -0,0 +1,23 @@
+const homeScreen = document.getElementById("homeScreen");
+const cameraScreen = document.getElementById("cameraScreen");
+
+const cameraApp = document.getElementById("cameraApp");
+const backBtn = document.getElementById("backBtn");
+
+const captureBtn = document.getElementById("captureBtn");
+const scanBtn = document.getElementById("scanBtn");
+const filterBtn = document.getElementById("filterBtn");
+const galleryBtn = document.getElementById("galleryBtn");
+
+const cameraImage = document.getElementById("cameraImage");
+const slide = document.getElementById("slide");
+const scannerFrame = document.querySelector(".scanner-frame");
+
+// ABRIR CAMERA
+
+cameraApp.addEventListener("click", () => {
+
+    homeScreen.style.display = "none";
+    cameraScreen.style.display = "flex";
+
+});
